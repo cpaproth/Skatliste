221e230c0f3919146eb314a3cfb8c1e7f0ba2e1f
Carsten Paproth <carpa@freenet.de>
2023-06-19 02:09:46 +0200

    first successful camera tests on real hardware

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,47 +1,114 @@
-//Copyright (c) 2023 Carsten Paproth - Licensed under MIT License
+//Copyright (C) 2023 Carsten Paproth - Licensed under MIT License
 
 #define CNFG_IMPLEMENTATION
 #include "external/rawdraw_sf.h"
 
 #include <string>
-#include <sstream>
 #include <vector>
 #include <iostream>
 
 using namespace std;
-string text;
 
-class mybuf : streambuf {
-    mybuf() {
-        cout.rdbuf(this);
-    }
-    virtual int_type overflow(int_type c) override {
-        text.append(1, (char)c);
+class coutbuf : streambuf {
+    streambuf* oldbuf;
+    string buffer;
+    int overflow(int c) {
+        if (c != 8)
+            buffer.append(1, (char)c);
+        else if (!buffer.empty())
+            buffer.pop_back();
+        while (count(buffer.begin(), buffer.end(), '\n') > 30)
+            buffer.erase(0, buffer.find('\n') + 1);
         return c;
     }
+public:
+    coutbuf() {
+        oldbuf = cout.rdbuf(this);
+    }
+    ~coutbuf() {
+        cout.rdbuf(oldbuf);
+    }
+    const char* c_str() {
+        return buffer.c_str();
+    }
 };
-//mybuf buf();
 
 #include <camera/NdkCameraManager.h>
 #include <camera/NdkCameraDevice.h>
-//#include <camera/NdkCameraCaptureSession.h>
-//#include <camera/NdkCameraMetadata.h>
 #include <media/NdkImageReader.h>
 
 
-vector<uint8_t> mem(640 * 480 * 4);
+void printCamProps(ACameraManager *cameraManager, const char *id) {
+    // exposure range
+    ACameraMetadata *metadataObj;
+    ACameraManager_getCameraCharacteristics(cameraManager, id, &metadataObj);
 
+    ACameraMetadata_const_entry entry = {0};
+    ACameraMetadata_getConstEntry(metadataObj, ACAMERA_SENSOR_INFO_EXPOSURE_TIME_RANGE, &entry);
+
+    int64_t minExposure = entry.data.i64[0];
+    int64_t maxExposure = entry.data.i64[1];
+    cout << "exposure: "<<minExposure<<" - "<< maxExposure<<endl;
+    ////////////////////////////////////////////////////////////////
+
+    // sensitivity
+    ACameraMetadata_getConstEntry(metadataObj,
+                                  ACAMERA_SENSOR_INFO_SENSITIVITY_RANGE, &entry);
+
+    int32_t minSensitivity = entry.data.i32[0];
+    int32_t maxSensitivity = entry.data.i32[1];
+
+    cout<<"sensitivity: "<<minSensitivity<<" - "<<maxSensitivity<<endl;
+    ////////////////////////////////////////////////////////////////
+
+    // JPEG format
+    ACameraMetadata_getConstEntry(metadataObj,
+                                  ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS, &entry);
+
+    for (int i = 0; i < entry.count; i += 4)
+    {
+        // We are only interested in output streams, so skip input stream
+        int32_t input = entry.data.i32[i + 3];
+        if (input)
+            continue;
+
+        int32_t format = entry.data.i32[i + 0];
+        if (format == AIMAGE_FORMAT_YUV_420_888)
+        {
+            int32_t width = entry.data.i32[i + 1];
+            int32_t height = entry.data.i32[i + 2];
+            cout<<"maxsize="<<width<<" x "<<height<<endl;
+        }
+    }
+
+    // cam facing
+    ACameraMetadata_getConstEntry(metadataObj,
+                                  ACAMERA_SENSOR_ORIENTATION, &entry);
+
+    int32_t orientation = entry.data.i32[0];
+    cout<<"orientation: "<<orientation<<endl;
+
+    ACameraMetadata_getConstEntry(metadataObj, ACAMERA_LENS_FACING, &entry);
+    cout<<"backfacing: "<<(int)entry.data.u8[0]<<endl;
+
+}
+
+
+
+vector<uint8_t> mem(640 * 480 * 4);
 void captureImage() {
     ACameraManager* cameraManager = ACameraManager_create();
     ACameraIdList* cameraIdList = nullptr;
     ACameraManager_getCameraIdList(cameraManager, &cameraIdList);
+
+    printCamProps(cameraManager, cameraIdList->cameraIds[0]);
 
     ACameraDevice* cameraDevice = nullptr;
     ACameraDevice_stateCallbacks cameraDeviceCallbacks{0, [](void*, ACameraDevice*){}, [](void*, ACameraDevice*, int) {}};
     ACameraManager_openCamera(cameraManager, cameraIdList->cameraIds[0], &cameraDeviceCallbacks, &cameraDevice);
 
     AImageReader* imageReader = nullptr;
-    AImageReader_new(640, 480, AIMAGE_FORMAT_RGBA_8888, 1, &imageReader);
+    AImageReader_new(640, 480, AIMAGE_FORMAT_YUV_420_888, 1, &imageReader);
 
     AImageReader_ImageListener listener{
             .context = nullptr,
@@ -52,16 +119,17 @@ void captureImage() {
                 int len = 0;
                 AImage_getPlaneData(image, 0, &data, &len);
 
-                for (unsigned i = 0; i < mem.size() && i < len; i += 4) {
-                    mem[i + 0] = data[i + 3];
-                    mem[i + 1] = data[i + 2];
-                    mem[i + 2] = data[i + 1];
-                    mem[i + 3] = data[i + 0];
-                }
+                int32_t w,h;
+                AImage_getWidth(image, &w);
+                AImage_getHeight(image, &h);
+                //cout<<w<<" x "<<h<<endl;
 
-                stringstream str;
-                str<<len;
-                //text="huch " + str.str();
+                for (unsigned i = 0; i < 640 * 480 && i < len; i++) {
+                    mem[4 * i] = 255;
+                    mem[4 * i + 1] = data[i];
+                    mem[4 * i + 2] = data[i];
+                    mem[4 * i + 3] = data[i];
+                }
 
                 AImage_delete(image);
             }
@@ -70,6 +138,16 @@ void captureImage() {
 
     ANativeWindow* window = nullptr;
     AImageReader_getWindow(imageReader, &window);
+    ANativeWindow_acquire(window);
+
+    ACaptureRequest* request = nullptr;
+    ACameraOutputTarget* jpegTarget = nullptr;
+    ACameraDevice_createCaptureRequest(cameraDevice, TEMPLATE_PREVIEW, &request);
+
+    ACameraOutputTarget_create(window, &jpegTarget);
+    ACaptureRequest_addTarget(request, jpegTarget);
+
+
 
     ACameraCaptureSession_stateCallbacks sessionStateCallbacks{
             .context = nullptr,
@@ -77,7 +155,6 @@ void captureImage() {
             .onReady = [](void*, ACameraCaptureSession*) {},
             .onActive = [](void*, ACameraCaptureSession*) {}
     };
-
     ACaptureSessionOutput* output = nullptr;
     ACaptureSessionOutputContainer* outputs = nullptr;
     ACameraCaptureSession* session = nullptr;
@@ -86,13 +163,6 @@ void captureImage() {
     ACaptureSessionOutputContainer_add(outputs, output);
     ACameraDevice_createCaptureSession(cameraDevice, outputs, &sessionStateCallbacks, &session);
 
-    ACaptureRequest* request = nullptr;
-    ACameraOutputTarget* jpegTarget = nullptr;
-    ACameraDevice_createCaptureRequest(cameraDevice, TEMPLATE_PREVIEW, &request);
-
-    ANativeWindow_acquire(window);
-    ACameraOutputTarget_create(window, &jpegTarget);
-    ACaptureRequest_addTarget(request, jpegTarget);
 
     ACameraCaptureSession_captureCallbacks captureCallbacks {
             .context = nullptr,
@@ -122,26 +192,19 @@ void captureImage() {
     //ACameraManager_delete(cameraManager);
 }
 
-
-
 void HandleKey( int keycode, int bDown ) {
     if (bDown && keycode > 0)
-        //cout << (char)keycode;
-        text.push_back(keycode);
-    else if (bDown && keycode == -67 && !text.empty())
-        text.pop_back();
+        cout << (char)keycode;
+    else if (bDown && keycode == -67)
+        cout << '\b';
 }
 void HandleButton( int x, int y, int button, int bDown ) {
-    stringstream str;
-    str << x << " " << y << " " << button << " " << bDown;
-    text = str.str();
+    cout << x << " " << y << " " << button << " " << bDown << endl;
     if (x > 100 && x < 200 && y > 100 && y < 200)
         AndroidDisplayKeyboard(!bDown);
 }
 void HandleMotion( int x, int y, int mask ) {
-    stringstream str;
-    str << x << " " << y << " " << mask;
-    text = str.str();
+    cout << x << " " << y << " " << mask << endl;
 }
 void HandleDestroy() {}
 void HandleResume() {}
@@ -149,66 +212,34 @@ void HandleSuspend() {}
 void HandleWindowTermination() {}
 
 int main(int, char**) {
+    coutbuf buf;
+
     if (!AndroidHasPermissions("CAMERA"))
         AndroidRequestAppPermissions("CAMERA");
 
     captureImage();
 
     CNFGSetupFullscreen("Skatliste", 0);
+    CNFGSetLineWidth(1);
+
     AndroidDisplayKeyboard(true);
 
+    while (CNFGHandleInput()) {
 
-    int count = 0;
-    while(CNFGHandleInput())
-    {
-
-        CNFGBGColor = 0x000080ff; //Dark Blue Background
+        CNFGBGColor = 0x000080ff;
 
         short w, h;
         CNFGClearFrame();
-        CNFGGetDimensions( &w, &h );
 
-        //Change color to white.
+        CNFGBlitImage((uint32_t*)mem.data(), 100, 100, 640, 480);
+
+        //CNFGGetDimensions( &w, &h );
+
         CNFGColor( 0xffffffff );
+        CNFGPenX = 20;
+        CNFGPenY = 20;
+        CNFGDrawText(buf.c_str(), 4);
 
-        CNFGSetLineWidth(count++ % 10 + 10);
-        CNFGPenX = 100;
-        CNFGPenY = 1000;
-        CNFGDrawText("Hello, World", 20);
-        CNFGPenY = 1100;
-        CNFGDrawText(text.c_str(), 20);
-        //Draw a white pixel at 30, 30
-        CNFGTackPixel( 30, 30 );
-
-        //Draw a line from 50,50 to 100,50
-        CNFGTackSegment( 50, 50, 100, 50 );
-
-        //Dark Red Color Select
-        CNFGColor( 0x800000FF );
-
-        //Draw 50x50 box starting at 100,50
-        CNFGTackRectangle( 100, 50, 150, 100 );
-
-        //Bright Purple Select
-        CNFGColor( 0x800000FF );
-
-        //Draw a triangle
-        RDPoint points[3] = { { 30, 36}, {20, 50}, { 40, 50 } };
-        CNFGTackPoly( points, 3 );
-
-        //Draw a bunch of random pixels as a blitted image.
-        {
-            static uint32_t data[640 * 640];
-            int x, y;
-
-            for (y = 0; y < 640; y++)
-                for (x = 0; x < 640; x++)
-                    data[x + y * 640] = 0xff | (rand() << 8);
-            CNFGBlitImage(data, 150, 30, 640, 640);
-
-            CNFGBlitImage((uint32_t*)mem.data(), 150, 1500, 640, 480);
-        }
-        //Display the image and wait for time to display next frame.
         CNFGSwapBuffers();
     }
 }
